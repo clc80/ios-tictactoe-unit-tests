9e8ae52c279f10ccc5b445e50c2ed477820a9321
Claudia Maciel <clc_80@yahoo.com>
2020-05-25 15:36:50 -0700

    Added a test for assuring ourselves that the game board is empty when it is created

--- TicTacToeTests/GameBoardTests.swift
+++ TicTacToeTests/GameBoardTests.swift
@@ -7,8 +7,26 @@
 //
 
 import XCTest
+@testable import TicTacToe
 
 class GameBoardTests: XCTestCase {
 
-    
+    func testCreatingEmptyBoard() {
+        let board = GameBoard()
+        
+        //XCTAssert(board[(x: 0, y:0)] == nil)
+        
+        // Check the first row
+        XCTAssertNil(board[(x: 0, y: 0)])
+        XCTAssertNil(board[(x: 1, y: 0)])
+        XCTAssertNil(board[(x: 2, y: 0)])
+        
+        XCTAssertNil(board[(x: 0, y: 1)])
+        XCTAssertNil(board[(x: 1, y: 1)])
+        XCTAssertNil(board[(x: 2, y: 1)])
+        
+        XCTAssertNil(board[(x: 0, y: 2)])
+        XCTAssertNil(board[(x: 1, y: 2)])
+        XCTAssertNil(board[(x: 2, y: 2)])
+    }
 }
