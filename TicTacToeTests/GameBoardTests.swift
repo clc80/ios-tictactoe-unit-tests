7e0e86a07ce243cf864991adbf15a59691e9972b
Claudia Maciel <clc_80@yahoo.com>
2020-05-25 14:54:13 -0700

    Added GameBoardTests to the unite testing target

---
+++ TicTacToeTests/GameBoardTests.swift
@@ -0,0 +1,14 @@
+//
+//  GameBoardTests.swift
+//  TicTacToeTests
+//
+//  Created by Claudia Contreras on 5/25/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class GameBoardTests: XCTestCase {
+
+    
+}
