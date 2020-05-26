e4ba3aaf4d77d99e9e858b103f229be8277fa451
Claudia Maciel <clc_80@yahoo.com>
2020-05-25 17:54:21 -0700

    Added tests and implementation for checking if a win is possible on the first column

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -7,12 +7,26 @@
 //
 
 import XCTest
+@testable import TicTacToe
 
 class GameAITests: XCTestCase {
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testWinCheckingVertical0() throws {
+        var board = GameBoard()
+        
+        try!board.place(mark: .x, on: (0, 0))
+        try!board.place(mark: .o, on: (1, 0))
+        
+        try!board.place(mark: .x, on: (0, 1))
+        try!board.place(mark: .x, on: (1, 1))
+        
+        XCTAssertFalse(game(board: board, isWonBy: .x))
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+        
+        try!board.place(mark: .x, on: (0, 2))
+
+        XCTAssertTrue(game(board: board, isWonBy: .x))
+        XCTAssertFalse(game(board: board, isWonBy: .o))
     }
 
 
